2f71ca466dcabf80898c25ba305418e22045133e
Abiram R <abiram.r1010@gmail.com>
2025-12-23 15:51:40 +0530

    Remove model loading from gradcam for cloud deployment

--- gradcam.py
+++ gradcam.py
@@ -1,18 +1,10 @@
 import tensorflow as tf
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # -----------------------------
-# LOAD MODEL
-# -----------------------------
-MODEL_PATH = "model/pneumonia_model.h5"
-model = load_model(MODEL_PATH)
-
-# -----------------------------
-# IMAGE PREPROCESSING FUNCTION
+# IMAGE PREPROCESSING
 # -----------------------------
 def preprocess_image(img_path, img_size=224):
     img = image.load_img(img_path, target_size=(img_size, img_size))
@@ -24,12 +16,15 @@ def preprocess_image(img_path, img_size=224):
 # -----------------------------
 # GRAD-CAM FUNCTION
 # -----------------------------
-def generate_gradcam(img_path, last_conv_layer_name="conv5_block3_out"):
+def generate_gradcam(model, img_path, last_conv_layer_name="conv5_block3_out"):
     img_array = preprocess_image(img_path)
 
     grad_model = tf.keras.models.Model(
-        [model.inputs],
-        [model.get_layer(last_conv_layer_name).output, model.output]
+        inputs=model.inputs,
+        outputs=[
+            model.get_layer(last_conv_layer_name).output,
+            model.output
+        ]
     )
 
     with tf.GradientTape() as tape:
@@ -40,16 +35,15 @@ def generate_gradcam(img_path, last_conv_layer_name="conv5_block3_out"):
     pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
 
     conv_outputs = conv_outputs[0]
-    heatmap = conv_outputs @ pooled_grads[..., tf.newaxis]
-    heatmap = tf.squeeze(heatmap)
+    heatmap = tf.reduce_sum(conv_outputs * pooled_grads, axis=-1)
 
-    heatmap = np.maximum(heatmap, 0)
-    heatmap /= np.max(heatmap)
+    heatmap = tf.maximum(heatmap, 0)
+    heatmap /= tf.reduce_max(heatmap) if tf.reduce_max(heatmap) != 0 else 1
 
-    return heatmap
+    return heatmap.numpy()
 
 # -----------------------------
-# OVERLAY HEATMAP ON IMAGE
+# OVERLAY HEATMAP
 # -----------------------------
 def overlay_heatmap(img_path, heatmap, alpha=0.4):
     img = cv2.imread(img_path)
@@ -61,26 +55,3 @@ def overlay_heatmap(img_path, heatmap, alpha=0.4):
 
     superimposed_img = cv2.addWeighted(heatmap, alpha, img, 1 - alpha, 0)
     return superimposed_img
-
-# -----------------------------
-# MAIN EXECUTION
-# -----------------------------
-if __name__ == "__main__":
-    image_path = "sample_xray.jpg"  # change this path
-
-    heatmap = generate_gradcam(image_path)
-    cam_image = overlay_heatmap(image_path, heatmap)
-
-    plt.figure(figsize=(10,4))
-
-    plt.subplot(1,2,1)
-    plt.title("Original X-ray")
-    plt.imshow(cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB))
-    plt.axis("off")
-
-    plt.subplot(1,2,2)
-    plt.title("Grad-CAM Heatmap")
-    plt.imshow(cv2.cvtColor(cam_image, cv2.COLOR_BGR2RGB))
-    plt.axis("off")
-
-    plt.show()
